3c4de92d458a6461e820cff613cc03df5c0613d2
Fedya <firdavs.prog@gmail.com>
2020-12-02 10:43:34 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/* eslint-disable prefer-const */
+'use strict';
+
+
+let cotegory = 'cat';
+const user = 'Max',
+   sum = 'Pro';
+
+alert(`Hello, ${user}, ${sum}`);
+
+console.log(2 * 2 + 2 != '6');
+
+
